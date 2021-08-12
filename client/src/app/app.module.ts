75a6fa77e8ddb10992aa63bb25564be6b15fb8d9
Hkcn90 <hkcn90@gmail.com>
2021-08-12 23:13:40 +0530

    Template Creation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './nav/sidebar/sidebar/sidebar.component';
+import { ToolbarComponent } from './nav/toolbar/toolbar/toolbar.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DasboardComponent } from './dasboard/dasboard.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'home', component: HomeComponent, 
+    children: [
+      { path: 'dashboard', component: DasboardComponent },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    LoginComponent,
+    HomeComponent,
+    DasboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
